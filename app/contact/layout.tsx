7339f704f89a731dcae3b4cca43fa3ffdd651e89
ericjiji3 <eric.ji3333@gmail.com>
2024-01-06 17:15:28 -0600

    adding email service

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,5 +1,17 @@
 import Footer from 'components/layout/footer';
+import type { Metadata } from 'next';
 import { Suspense } from 'react';
+
+export async function generateMetadata({
+  params
+}: {
+  params: { page: string };
+}): Promise<Metadata> {
+  return {
+    title: 'CONTACT US',
+    description: 'Leave us a message and we will get back to you as soon as possible.'
+  };
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,35 +1,16 @@
-import type { Metadata } from 'next';
+import Form from 'components/form';
+
 import Image from 'next/image';
 import Bg from '../../public/DEMMO01.png';
 export const runtime = 'edge';
 export const revalidate = 43200; // 12 hours in seconds
 
-
-
-export async function generateMetadata({
-    params
-  }: {
-    params: { page: string };
-  }): Promise<Metadata> {
-    return {
-      title: 'CONTACT US',
-      description: 'Leave us a message and we will get back to you as soon as possible.',
-    };
-}
-
 export default async function ContactPage() {
-
-return (
+  return (
     <>
-    <Image src={Bg} width={500} height={500} alt="oops"/>
-    <form method="post" action="/contact#contact_form" id="contact_form" acceptCharset="UTF-8" className="contact-form">
-        <input type="hidden" name="form_type" value="contact" />
-        <input type="hidden" name="utf8" value="✓" />
-        <input type="text" name="email"/>
-        <textarea name="message"></textarea>
-        <input type="submit" value="Submit">
-    </form>
-    {/* <p className="text-sm italic">
+      <Image src={Bg} width={500} height={500} alt="oops" />
+      <Form />
+      {/* <p className="text-sm italic">
         {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
         year: 'numeric',
         month: 'long',
@@ -37,5 +18,5 @@ return (
         }).format(new Date(page.updatedAt))}.`}
     </p> */}
     </>
-);
+  );
 }
